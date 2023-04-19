38d1ea4817dc9b347183385683e54bafe136b744
jackfreeman8754 <104140255+jackfreeman8754@users.noreply.github.com>
2023-04-19 20:23:53 +0300

    Update cc_sbu_dataset.py

--- cc_sbu_dataset.py
+++ cc_sbu_dataset.py
@@ -9,14 +9,11 @@ class CCSBUDataset(BaseDataset):
     def __init__(self, vis_processor, text_processor, location):
         super().__init__(vis_processor=vis_processor, text_processor=text_processor)
 
-        self.inner_dataset = wds.DataPipeline(
             wds.ResampledShards(location),
             wds.tarfile_to_samples(handler=wds.warn_and_continue),
             wds.shuffle(1000, handler=wds.warn_and_continue),
             wds.decode("pilrgb", handler=wds.warn_and_continue),
             wds.to_tuple("jpg", "json", handler=wds.warn_and_continue),
-            wds.map_tuple(self.vis_processor, handler=wds.warn_and_continue),
-            wds.map(self.to_dict, handler=wds.warn_and_continue),
         )
 
     def to_dict(self, sample):
@@ -25,8 +22,6 @@ class CCSBUDataset(BaseDataset):
             "text_input": self.text_processor(sample[1]["caption"]),
         }
 
-
-class CCSBUAlignDataset(CaptionDataset):
 
     def __getitem__(self, index):
 
